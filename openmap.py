a4f49506d45f76c604d4a56a79e8a5db4155a3ce
mamorski <igor.mamorski@gmail.com>
2018-03-18 21:35:37 +0200

    added comments to all files

--- openmap.py
+++ openmap.py
@@ -1,18 +1,12 @@
 import os
 import sys
 import re
-# google response to wrong address
-# {
-#    "results" : [],
-#    "status" : "ZERO_RESULTS"
-# }
+
+# get route and open it on Google Maps in default browser
 def openmap(route, index):
-    # print(route[0])
-    print(index)
-    # print(route[1])
-    print(route[index])
     url = 'https://www.google.com/maps/dir/?api=1&'
     length = len(route)
+    
     if len == 1:
         str = re.sub(r'\s+', '+', route[index][0])
         url += 'origin=' + str + '&destination=Ort+Braude+College&travelmode=driving'
@@ -28,12 +22,6 @@ def openmap(route, index):
                 url += str
             i += 1
 
-    # key = 'AIzaSyDMIxx6GqU9oBiIop9DU3rpHPWIVg5-GP4'
-    # google_url = 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=' + key
-    # url = 'https://www.google.com/maps/dir/?api=1&origin=Paris,' \
-    #       'France&destination=Cherbourg,France&travelmode=driving&waypoints=Versailles,' \
-    #       'France%7CChartres,France%7CLe+Mans,France%7CCaen,France'
-    print(url)
     if sys.platform=='win32':
         os.startfile(url)
     elif sys.platform =='darwin':
--- validate_address.py
+++ validate_address.py
@@ -3,26 +3,17 @@ import time
 from urllib.request import urlopen
 import re
 
-
+# get address and check it with Google API if the address is valid
 def validate_address(address):
-    # The maps_key defined below isn't a valid Google Maps API key.
-    # You need to get your own API key.
-    # See https://developers.google.com/maps/documentation/timezone/get-api-key
+
     if re.search(r'israel', address, re.I):
         my_address = re.sub(r'\s+', '+', address)
     else:
         my_address = 'israel+' + address
         my_address = re.sub(r'\s+', '+', my_address)
+
     maps_key = 'AIzaSyDMIxx6GqU9oBiIop9DU3rpHPWIVg5-GP4'
     url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + my_address + '&key=' + maps_key
-
-    # This joins the parts of the URL together into one string.
-    # url = timezone_base_url + '?' + urllib.parse.urlencode({
-    #     'location':  "%s,%s" % (lat, lng),
-    #     'timestamp': timestamp,
-    #     'key':       maps_key,
-    # })
-
     current_delay = 0.1  # Set the initial retry delay to 100ms.
     max_delay = 2  # Set the maximum retry delay to 1 hour.
 
